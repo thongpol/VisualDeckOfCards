b349a56eefbd62d698b5e54380155734fdcbfbe6
thongpol <78428236+thongpol@users.noreply.github.com>
2021-05-01 21:54:51 +0700

    Updated with all code and images from the video series

--- src/Card.java
+++ src/Card.java
@@ -1,22 +1,17 @@
-import javafx.scene.image.Image;
-
 import java.util.Arrays;
 import java.util.List;
 
 public class Card {
     private String faceName, suit;
-    private Image image;
+//    private Image image;
+
 
     public Card(String faceName, String suit) {
         setFaceName(faceName);
         setSuit(suit);
-        String fileName = faceName + "_of_"+suit+".png";
-        image = new Image("./images/"+fileName);
     }
 
-    public String getFaceName() {
-        return faceName;
-    }
+
 
     /**
      * This method returns a list of face names that are valid for
@@ -28,16 +23,8 @@ public class Card {
                             "queen","king","ace");
     }
 
-    public Image getImage() {
-        return image;
-    }
-
-    public void setImage(Image image) {
-        this.image = image;
-    }
-
     /**
-     * This method will validate the argument and set the instance variable
+     * This method will valid the argument and set the instance variable
      * @param faceName 2,3,4,5,6,7,8,9,10,jack,queen,king
      */
     public void setFaceName(String faceName) {
@@ -47,12 +34,8 @@ public class Card {
         if (validFaceNames.contains(faceName))
             this.faceName = faceName;
         else
-            throw new IllegalArgumentException("Valid face names are: "+
+            throw new IllegalArgumentException("Valid face names are"+
                             validFaceNames);
-    }
-
-    public String getSuit() {
-        return suit;
     }
 
     /**
@@ -67,11 +50,11 @@ public class Card {
     public void setSuit(String suit) {
         List<String> validSuits = getValidSuits();
         suit = suit.toLowerCase();
-
+        
         if (validSuits.contains(suit))
             this.suit = suit;
         else
-            throw new IllegalArgumentException("valid suits are: "+ validSuits);
+            throw new IllegalArgumentException("valid suits are"+ validSuits);
     }
 
     public String toString()
